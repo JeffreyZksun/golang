b5421bbb24c154cbe27d4f4a2e262fdfb46c518b
Jeffrey Sun <jeffrey.zksun@gmail.com>
2013-11-15 15:12:22 +0800

    init

---
+++ rpc/server.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+    //"crypto/tls"
+	"net"
+	"net/rpc"
+    "log"
+    //"net/http"
+)
+
+type Args struct {
+	X, Y int
+}
+
+type Calculator struct{}
+
+func (t *Calculator) Add(args *Args, reply *int) error {
+	*reply = args.X + args.Y
+	return nil
+}
+
+func main(){
+    cal := new(Calculator)
+    rpc.Register(cal)
+    //rpc.HandleHTTP()
+    listener, e := net.Listen("tcp", ":1234")
+    if e != nil {
+        log.Fatal("listen error:", e)
+    }
+    
+    for {
+        if conn, err := listener.Accept(); err != nil {
+            log.Fatal("accept error: " + err.Error())
+        } else {
+            log.Printf("new connection established\n")
+            go rpc.ServeConn(conn)
+        }
+    }
+}
